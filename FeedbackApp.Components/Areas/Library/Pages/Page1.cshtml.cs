61c989eda963d65d8c8031d7ad9e2f80a5525f3f
BumindaNawagamuwa <buminda.nawagamuwa@health.telstra.com>
2022-06-27 10:27:17 +1000

    How to create a RazroClassLibrary project

---
+++ FeedbackApp.Components/Areas/Library/Pages/Page1.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FeedbackApp.Components.Library.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
