079c1f50787fb0bcab6e8cbc0ca11f332998d7d4
BumindaNawagamuwa <buminda.nawagamuwa@health.telstra.com>
2022-06-28 14:56:49 +1000

    Add DTO for feedback

---
+++ FeedbackApp.Shared/Models/FeedbackDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedbackApp.Shared.Models
+{
+    public class FeedbackDTO
+    {
+        public int FeedbackId { get; set; }
+        public string Feedback { get; set; }
+    }
+}
